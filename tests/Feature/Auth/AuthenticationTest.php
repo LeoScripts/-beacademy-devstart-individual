2f7d555007820541231a331a4c71d2b969c5633d
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-17 16:17:12 -0300

    test: can not autentication with invalid pass

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -29,4 +29,14 @@ class AuthenticationTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_users_can_not_authenticate_with_invalid_password()
+    {
+        $user = User::factory()->create();
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertGuest();
+    }
 }
