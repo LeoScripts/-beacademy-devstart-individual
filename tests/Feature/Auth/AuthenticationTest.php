b6f3496757c0e0f7b5fa02a415d8140978dbc812
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-17 16:13:36 -0300

    test: autentication and redirect route home

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,7 +14,18 @@ class AuthenticationTest extends TestCase
     public function test_login_screen_can_be_rendered()
     {
         $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
 
+    public function test_users_can_authenticate_using_the_login_screen()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => '123456',
+        ]);
+        $this->actingAs($user);
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 
