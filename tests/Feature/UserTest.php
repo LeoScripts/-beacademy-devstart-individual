b32f9916dcf2ba4a5aea3589dde9ea36c54e62b5
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-07-17 16:32:08 -0300

    test: rendered view user_show

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
@@ -16,6 +17,18 @@ class UserTest extends TestCase
     public function test_rendered_home()
     {
         $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_user_show()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => '123456',
+        ]);
+        $this->actingAs($user);
+        $response = $this->get("/users/$user->id");
         $response->assertStatus(200);
     }
 }
